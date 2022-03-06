12bb7815f93161f5f9cdb6da710e4d7d23b6dd46
8xhamhoc <tomatoquang@gmail.com>
2022-03-06 16:32:41 +0700

    Push builder pattern

---
+++ src/main/java/org/quangphan/design/patterns/builder/car/Builder.java
@@ -0,0 +1,13 @@
+package org.quangphan.design.patterns.builder.car;
+
+import org.quangphan.design.patterns.builder.car.features.*;
+
+public interface Builder {
+
+    void setCarType(CarType type);
+    void setSeats(int seats);
+    void setEngine(Engine engine);
+    void setTransmission(Transmission transmission);
+    void setTripComputer(TripComputer tripComputer);
+    void setGPSNavigator(GPSNavigator gpsNavigator);
+}
---
+++ src/main/java/org/quangphan/design/patterns/builder/car/features/CarType.java
@@ -0,0 +1,6 @@
+package org.quangphan.design.patterns.builder.car.features;
+
+public enum CarType {
+
+    CITY_CAR, SPORTS_CAR, SUV
+}
